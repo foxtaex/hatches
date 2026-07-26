d83eead083c9a70cfcbe2476549545ac93b79fc7
Foxy <47433349+foxtaex@users.noreply.github.com>
2026-07-26 06:25:21 +0200

    Restet
    Alles gelöscht

--- prisma/migrations/20260722094000_add_card_doc_link/migration.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "Card" ADD COLUMN "linkedDocId" INTEGER REFERENCES "Doc"("id") ON DELETE SET NULL;
-ALTER TABLE "Card" ADD COLUMN "linkedDocMode" TEXT;
-CREATE INDEX "Card_linkedDocId_idx" ON "Card"("linkedDocId");
